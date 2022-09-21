cc4cdd03ce30b92b048a8fc2dade69692388f6de
omPatel1718 <67968273+omPatel1718@users.noreply.github.com>
2022-09-21 12:33:37 -0500

    food java class

---
+++ app/src/main/java/com/example/menu/Food.java
@@ -0,0 +1,48 @@
+package com.example.menu;
+
+public class Food {
+    private String name;
+    private double price;
+    private String desc;
+    private int imageResourceID;
+
+    //Code --> Generate
+    public Food(String name, double price, String desc, int imageResourceID) {
+        this.name = name;
+        this.price = price;
+        this.desc = desc;
+        this.imageResourceID = imageResourceID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getImageResourceID() {
+        return imageResourceID;
+    }
+
+    public void setImageResourceID(int imageResourceID) {
+        this.imageResourceID = imageResourceID;
+    }
+}
