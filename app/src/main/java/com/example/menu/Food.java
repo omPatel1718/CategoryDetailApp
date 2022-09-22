a65c465758c8ec33601003dca726ef9ad8a55e18
omPatel1718 <67968273+omPatel1718@users.noreply.github.com>
2022-09-22 12:51:30 -0500

    images

--- app/src/main/java/com/example/menu/Food.java
+++ app/src/main/java/com/example/menu/Food.java
@@ -14,8 +14,27 @@ public class Food {
         this.imageResourceID = imageResourceID;
     }
 
-    //don't use Generate
+    public static final Food[] mySnacks = {
+            //new Food("Nachos", 3.99, "snak", ),
+            new Food("Pretzel", 4.99, "snak", R.drawable.pretzel),
+            new Food("Fries", 1.50, "snak", R.drawable.fries)
+    };
+
+    public static final Food[] myDrinks = {
+            //new Food("Water", 0.99, "Water", ),
+            //new Food("Juice", 1.99, "Orange, Apple, Lemon, Pineapple", )
+            //new Food("Soda", 1.99, "insert list of brands here"
+    };
+
+    public static final Food[] myEntrees = {
+            //new Food("Sand", 0.99, "Water", ),
+            //new Food("Juice", 1.99, "Orange, Apple, Lemon, Pineapple", )
+            //new Food("Soda", 1.99, "insert list of brands here"
+    };
+
+
     //overrides default print
+    //don't use Generate
     public String toString(){
         return name;
     }
